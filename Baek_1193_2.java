6418e3b7c9e22be3e15f688794cbe3f2f7a38da4
eldks <42333313+eldks@users.noreply.github.com>
2021-04-06 23:31:48 +0900

    Add files via upload

---
+++ Baek_1193_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Baek_1193_2 {
+
+	public static void main(String[] args) {
+		Scanner src = new Scanner(System.in);
+		int num = src.nextInt();
+		int cross = 1;
+		int prev = 0;
+		
+		while (true) {
+			if (num <= (prev + cross)) {
+				if (cross % 2 == 1) {
+					System.out.print((cross - (num - prev - 1)) + "/"+(num-prev));
+					break;
+				}
+				else {
+					System.out.print((num - prev)+"/"+(cross-(num-prev-1)));
+					break;
+				}
+			}
+			else {
+				prev += cross;
+				cross++;
+			}
+		}
+
+	}
+
+}
